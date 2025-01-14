cc5c8db7b609cf87b71d933c60d3b4b8434ad519
nomadgirles <nomadgirles@users.noreply.github.com>
2025-01-14 08:04:54 +0000

    deploy: 510f47c853e2936aa45d30575aece5cbd38aad3e

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 console.log("외주소녀가 좋아하는 랜덤 게임~! 아파트 아파트");
 const config = {
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyCb-QYB5R3FliwVc83ZebW00cBWgiKRsCE',
+    authDomain: 'keyboardyst-57662.firebaseapp.com',
+    projectId: 'keyboardyst-57662',
+    storageBucket: 'keyboardyst-57662.appspot.com',
+    messagingSenderId: '918205709481',
+    appId: '1:918205709481:web:c05268524bf62ed3b8fdb8',
+    measurementId: 'G-255XD0RQTS',
   },
 };
