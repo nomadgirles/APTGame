097b7ce9cd231f46a8546576a174cb51428e91bf
nomadgirles <nomadgirles@users.noreply.github.com>
2025-01-14 07:45:27 +0000

    deploy: b94fb56a75a508fba0202a498cc7169459673f26

---
+++ config.js
@@ -0,0 +1,12 @@
+console.log("외주소녀가 좋아하는 랜덤 게임~! 아파트 아파트");
+const config = {
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
+};
